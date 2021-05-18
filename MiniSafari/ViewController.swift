936da96942510dd1e4c8e3c59e41fb2087724cbd
Sreenivassreee <ksreenivascse@gmail.com>
2021-05-18 23:53:12 +0530

    static data load

--- MiniSafari/ViewController.swift
+++ MiniSafari/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,WKNavigationDelegate {
+    var web = WKWebView()
+    override func loadView() {
+    
+        web.navigationDelegate = self
+        view = web
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        web.load(URLRequest(url: url))!
+        web.allowsBackForwardNavigationGestures = true
     }
 
 
