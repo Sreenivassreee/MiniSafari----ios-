34f365af5b3c941e08c36508943503813e87ffd6
Sreenivassreee <ksreenivascse@gmail.com>
2021-05-19 00:15:26 +0530

    add UIAlertController

--- MiniSafari/ViewController.swift
+++ MiniSafari/ViewController.swift
@@ -22,8 +22,26 @@ class ViewController: UIViewController,WKNavigationDelegate {
         let url = URL(string: "https://www.hackingwithswift.com")!
         web.load(URLRequest(url: url))!
         web.allowsBackForwardNavigationGestures = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "OPen", style: .plain, target: self, action: #selector(openTapped))
     }
-
+    
+    @objc func openTapped(){
+        let alert = UIAlertController(title: "Open ", message: nil , preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "apple.com", style: .default , handler: openPage))
+        alert.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(alert, animated: true, completion: nil)
+        
+    }
+    func openPage(action:UIAlertAction){
+        print(action.title)
+        if let w = action.title{
+            var url = URL(string: "https://"+w)!
+            web.load(URLRequest(url: url))
+        }
+    }
+    
+    
 
 }
 
